fb6400be2a77ed6a694de0c1a9268badde2360f5
Taeil-Nam <tnam@student.42seoul.kr>
2023-08-22 16:46:24 +0900

    Modify: 충돌 메세지 삭제

--- src/utils/utils_1.c
+++ src/utils/utils_1.c
@@ -8,23 +8,15 @@ unsigned int	custom_rand(void)
 	const unsigned long long int	m = 1ULL << 62;
 
 	seed = (a * seed + c) % m;
-<<<<<<< HEAD
 	return ((unsigned int)(seed >> 31));
-=======
-	return ((unsigned int)(seed >> 32));
->>>>>>> aaf162c6471155903583324caba62616e41d849b
 }
 
 double	random_double(void)
 {
-<<<<<<< HEAD
 	return (custom_rand() / (2147483647 + 1.0));
 }
 
 double	random_double_mm(double min, double max)
 {
 	return (min + ((max - min) * random_double()));
-=======
-	return (custom_rand() / (4294967295 + 1.0));
->>>>>>> aaf162c6471155903583324caba62616e41d849b
 }
