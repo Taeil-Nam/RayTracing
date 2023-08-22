152dcf8732d7f5d2e976f3b750729fa9c5eb9dfc
Taeil-Nam <tnam@student.42seoul.kr>
2023-08-22 15:36:02 +0900

    Modify: random_double() 함수 수정
    - RAND_MAX -> 4294967295로 변경.

--- src/utils/utils_1.c
+++ src/utils/utils_1.c
@@ -1,9 +1,17 @@
 #include <math.h>
-#include <stdlib.h>
 
-#define PHI 0x9e3779b9
+unsigned int	custom_rand(void)
+{
+	static unsigned long long int	seed = 1;
+	const unsigned long long int	a = 6364136223846793005ULL;
+	const unsigned long long int	c = 1ULL << 30;
+	const unsigned long long int	m = 1ULL << 62;
+
+	seed = (a * seed + c) % m;
+	return ((unsigned int)(seed >> 32));
+}
 
 double	random_double(void)
 {
-	return (rand() / (RAND_MAX + 1.0));
+	return (custom_rand() / (4294967295 + 1.0));
 }
