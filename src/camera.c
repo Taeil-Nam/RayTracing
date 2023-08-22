aaf162c6471155903583324caba62616e41d849b
Gunjoong Kim <rjsoownd0419@gmail.com>
2023-08-22 15:52:15 +0900

    Add : camera/parser 관련 파일 추가
    - camera 세팅과 관련된 camera.c 추가
    - parser.c와 parser_util.c 추가

---
+++ src/camera.c
@@ -0,0 +1,61 @@
+#include "minirt.h"
+
+double	degrees_to_radians(double degrees) {
+    return degrees * PI / 180.0;
+}
+
+t_vec3	get_lower_left_corner(t_camera *cam)
+{
+	t_vec3	ret;
+	t_vec3	half_h;
+	t_vec3	half_v;
+
+	half_h = vec3_mul_scalar(cam->horizontal, 0.5);
+	half_v = vec3_mul_scalar(cam->vertical, 0.5);
+	ret = vec3_sub(cam->center, half_h);
+	ret = vec3_sub(ret, half_v);
+	ret = vec3_sub(ret, cam->w);
+	return (ret);
+}
+
+void	set_camera_image(t_color background, double ratio, t_camera *cam)
+{
+	cam->a_background.x = background.x;
+	cam->a_background.y = background.y;
+	cam->a_background.z = background.z;
+	cam->a_ratio = ratio;
+}
+
+void	set_camera_pos(t_point3 look_from, t_vec3 dir, double hfov, t_camera *cam)
+{
+	t_vec3	vup;
+	double	viewport_h;
+	double	viewport_w;
+
+	vup.x = 0;
+	vup.y = 1.0;
+	vup.z = 0;
+	viewport_w = tan(degrees_to_radians(hfov) / 2) * 2.0;
+	viewport_h = viewport_w * (DEFAULT_IMAGE_HGT / DEFAULT_IMAGE_WID);
+	cam->w = vec3_unit(dir);
+	cam->u = vec3_unit(vec3_cross(vup, cam->w));
+	cam->v = vec3_cross(cam->w, cam->u);
+	cam->center = look_from;
+	cam->horizontal = vec3_mul_scalar(cam->u, viewport_w);
+	cam->vertical = vec3_mul_scalar(cam->v, viewport_h);
+	cam->lower_left_corner = get_lower_left_corner(cam);
+}
+
+t_ray	get_ray(t_camera *cam, double s, double t)
+{
+	t_ray	ret;
+
+	ret.orig = cam->center;
+	ret.dir.x = cam->lower_left_corner.x + cam->horizontal.x * s + \
+				cam->vertical.x * t - cam->center.x;
+	ret.dir.y = cam->lower_left_corner.y + cam->horizontal.y * s + \
+				cam->vertical.y * t - cam->center.y;
+	ret.dir.z = cam->lower_left_corner.z + cam->horizontal.z * s + \
+				cam->vertical.z * t - cam->center.z;
+	return (ret);
+}
---
+++ src/parser_util.c
@@ -0,0 +1,65 @@
+#include "minirt.h"
+
+int	count_element_2pt_arr(char **data)
+{
+	int	count;
+
+	while (*data)
+	{
+		count++;
+		data++;
+	}
+	return (count);
+}
+
+int	is_value_btw_255(int value, int *rgb_errno)
+{
+	if (value < 0 || value > 255)
+		rgb_errno = 1;
+	return (value);
+}
+
+// ft_atoi로 넘길 str 검사를 해봐야함
+int	data_to_rgb(char *str, t_color *rgb)
+{
+	char	**rgb_data;
+	int		value;
+	int		rgb_errno;
+
+	rgb_errno = 0;
+	rgb_data = ft_split(str, ',');
+	if (count_element_2pt_arr(rgb_data) != 3)
+	{
+		ft_double_free(rgb_data);
+		return (-1);
+	}
+	rgb->x = is_value_btw_255(ft_atoi(rgb_data[0]), &rgb_errno);
+	rgb->y = is_value_btw_255(ft_atoi(rgb_data[1]), &rgb_errno);
+	rgb->z = is_value_btw_255(ft_atoi(rgb_data[2]), &rgb_errno);
+	value = ft_atoi(rgb_data[1]);
+	ft_double_free(rgb_data);
+	if (rgb_errno == 1)
+		return (-1);
+	return (1);
+}
+
+int	data_to_point(char *str, t_point3 *point)
+{
+	char	**point_data;
+	int		atod_errno;
+
+	atod_errno = 0;
+	point_data = ft_split(str, ',');
+	if (count_element_2pt_arr(point_data) != 3)
+	{
+		ft_double_free(point_data);
+		return (-1);
+	}
+	point->x = ft_atod(point_data[0], &atod_errno);
+	point->y = ft_atod(point_data[1], &atod_errno);
+	point->z = ft_atod(point_data[2], &atod_errno);
+	ft_double_free(point_data);
+	if (atod_errno == ATOD_FORMAT_ERR)
+		return (-1);
+	return (1);
+}
