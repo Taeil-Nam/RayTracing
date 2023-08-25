171358e75db84a91a5ca3493e55597c4ff4de9d0
Taeil Nam (Tayden) <tnam@student.42seoul.kr>
2023-08-25 17:04:11 +0900

    Modify: libft의 ft_atoi 로직 수정 (#19)
    - 부호가 2개 이상인 경우 -1(error) 반환.
    - 결과 값이 INT32_MIN보다 작거나, INT32_MAX보다 큰 경우 -1(error) 반환.
    - 숫자가 아닌 문자가 들어있는 경우 -1(error) 반환.

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gunjkim <gunjkim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tnam <tnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 15:22:16 by gunjkim           #+#    #+#             */
-/*   Updated: 2023/03/14 15:19:08 by gunjkim          ###   ########.fr       */
+/*   Updated: 2023/08/25 15:42:07 by tnam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isspace(int c)
+static const char	*ft_isspace(const char *str)
 {
-	if ((c >= 9 && c <= 13) || c == 32)
-		return (1);
-	return (0);
+	while ((9 <= *str && *str <= 13) || *str == 32)
+		str++;
+	return (str);
 }
 
 int	ft_atoi(const char *str)
@@ -26,15 +26,23 @@ int	ft_atoi(const char *str)
 
 	sign = 1;
 	result = 0;
-	while (ft_isspace(*str))
-		str++;
-	if (*str == '-')
-		sign = -1;
+	str = ft_isspace(str);
 	if (*str == '-' || *str == '+')
-		str++;
-	while (ft_isdigit(*str))
 	{
-		result = result * 10 + (*str - '0');
+		if (*str == '-')
+			sign = -1;
+		str++;
+		if (*str == '\0' || *str == '+' || *str == '-')
+			return (-1);
+	}
+	while (*str)
+	{
+		if ('0' <= *str && *str <= '9')
+			result = result * 10 + (*str - '0');
+		else
+			return (-1);
+		if (result * sign < INT32_MIN || result * sign > INT32_MAX)
+			return (-1);
 		str++;
 	}
 	return (result * sign);
